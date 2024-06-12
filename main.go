9bc7fee4641912ca7b80d669ac64141acd1a1473
‘chidinma21’ <chidinmaonuora1@gmail.com>
2024-06-12 22:45:17 +0100

    Get event by ID

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Chidinma21/Events-Booking-API/db"
 	"github.com/Chidinma21/Events-Booking-API/models"
@@ -14,6 +15,8 @@ func main() {
 	server := gin.Default()
 	server.GET("/events", getEvents)
 	server.POST("/events", createEvent)
+	server.GET("/events/:id", getEvent)
+
 
 	server.Run(":8080")
 }
@@ -22,8 +25,23 @@ func getEvents(ctx *gin.Context) {
 	events, err := models.GetAllEvents()
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events"})
+		return
 	}
 	ctx.JSON(http.StatusOK, gin.H{"events": events})
+}
+
+func getEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64) 
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID"})
+		return
+	}	
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get event"})
+		return
+	}
+	ctx.JSON(http.StatusOK, event)
 }
 
 func createEvent(ctx *gin.Context) {
@@ -40,9 +58,10 @@ func createEvent(ctx *gin.Context) {
 	event.UserID = 1
 
 	err = event.Save()
-	
+
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events"})
+		return
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"message": "created event", "event": event})
--- models/events.go
+++ models/events.go
@@ -15,7 +15,7 @@ type Event struct {
 	UserID int
 }
 
-func (e Event) Save() error {
+func (e *Event) Save() error {
  // Add to DB later
  query := `
  INSERT INTO events(name, description, location, dateTime, user_id)
@@ -55,4 +55,16 @@ func GetAllEvents() ([]Event, error) {
 		events = append(events, event)
 	}
 	return events, nil
+}
+
+func GetEventByID(id int64) (*Event, error) {
+	query := `SELECT * FROM events WHERE id = ?`
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return nil, err
+	}
+	return &event, nil
 }
